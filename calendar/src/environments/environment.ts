7f55adfd1e5ea3e02cc886374f4542bf9acff56d
Soumyadeepta Das <50587378+soumyadeeptadas@users.noreply.github.com>
2021-08-28 20:20:11 +0530

    Add files via upload

---
+++ calendar/src/environments/environment.ts
@@ -0,0 +1,20 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+
+export const environment = {
+   production: false,
+  firebase: {
+    apiKey: "AIzaSyC5DfO4kxkhw1Q4RTdNpygKsAs2VRaB5oY",
+    authDomain: "calendarapp-sd.firebaseapp.com",
+    projectId: "calendarapp-sd",
+    storageBucket: "calendarapp-sd.appspot.com",
+    messagingSenderId: "539724572178",
+    appId: "1:539724572178:web:53994608e06daa7228dacb",
+    databaseURL:"https://calendarapp-sd-default-rtdb.firebaseio.com/",
+    measurementId: "G-3ZTJW9BBJ4"
+  }
+    
+  };
